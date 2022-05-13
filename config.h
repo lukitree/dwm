e3dca1a3d9eee9dd9fca457271eb39e2ee4e6524
lukitree <lukitree@gmail.com>
2022-05-13 10:30:36 -0600

    Change to a light theme

--- config.h
+++ config.h
@@ -25,8 +25,8 @@ static const char col_red[]         = "#ff0000";
 static const char col_lime[]        = "#007700";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_lime,  col_red },
+	[SchemeNorm] = { col_gray1, col_gray3, col_gray4 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan },
 };
 
 /* terminal to use */
