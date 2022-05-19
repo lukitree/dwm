7edf1c4c5247cf9138e69cf1f8fde9d2e350824c
lukitree <lukitree@gmail.com>
2022-05-19 16:04:49 -0600

    Move thunderbird to tag 9

--- config.h
+++ config.h
@@ -44,7 +44,7 @@ static const Rule rules[] = {
 	 */
 	/* class              instance    title          tags mask     iscentered   isfloating    monitor */
 	{ "Nvidia-settings",  NULL,       NULL,          0 ,           1,            1,           -1 },
-	{ "Thunderbird",      NULL,       NULL,          1 << 2,       0,            0,            0 },
+	{ "Thunderbird",      NULL,       NULL,          1 << 8,       0,            0,            0 },
 	{ "discord",          NULL,       "Discord",     0,            0,            0,            3 },
 	{ "Google-chrome",    NULL,       "Pandora",     0,            0,            0,            1 },
 	{ "Alacritty",        NULL,       "pianobar",    1 << 1,       1,            1,            1 },
